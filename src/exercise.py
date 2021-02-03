64b86d99f4e5bdbcc50d86697774d98477341773
Matthew O'Brien <matthew.o'brien@ysgolnantgwyn.co.uk>
2021-02-03 19:58:02 +0000

    Attempt 1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
+
+
+
 def main():
-    #write your code below this line
+   #write your code below this line
+   message = input ("write name:")
+   print("Hi " + message)
 
 if __name__ == '__main__':
-    main()
+   main()
