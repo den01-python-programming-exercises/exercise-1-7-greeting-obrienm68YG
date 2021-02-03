319d47e6903b2449a2af5df439c8330d49717305
Matthew O'Brien <matthew.o'brien@ysgolnantgwyn.co.uk>
2021-02-03 20:22:42 +0000

    attempt 4

--- src/exercise.py
+++ src/exercise.py
@@ -3,7 +3,7 @@
 
 def main():
    #write your code below this line
-  message = input ("write name:")
+  message = input ("write name ")
   print("Hi " + message)
 
 if __name__ == '__main__':
--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest`
 import src.exercise
 
 def test_exercise():
